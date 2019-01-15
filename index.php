d239291a051f2e71c019cd75b397849b3d260ac9
Jackry <Jackry@fordwood.com>
2019-01-15 12:38:27 -0800

    initial commit

---
+++ index.php
@@ -0,0 +1,305 @@
+<html>
+
+<head>
+<meta name="description" content="We offer FREE standard shipping on full wood kits within the Continental U.S. and have a 30 day money back guarantee. Reproduction wood parts for cars 1913 - 1936. Family owned and operated since 1964.">
+<meta name="google-site-verification" content="bvgp89PA0WSeFlO5gcG5YgCcLZuJkqC7B6X1i4dMjwU" />
+<meta name="keywords" content="Ford, FORD, T model, t model, A Model, a model, Model T parts, T bucket, ford wood, ford, ford parts, ford wood, 1913, 1914, 1915, 1916, 1917, 1918, 1919, 1920, 1921, 1922, 1923, 1924, 1925, 1926, 1927, 1928, 1929, 1930, 1931, 1932, 1933, 1934, 1935, 1936, 1936, Tudor, Fordor, tudor, fordor, four-door, two-door, Coupe, coupe, Coupelet, coupelet, Roadster, roadster, Touring, touring, Phaeton, phaeton, a-wood, A wood, antique, antique auto parts, antique ford part, ash, auto, automobile, briggs, Briggs, 155, 160, 165, 170, 1913-1936, Ford antique parts, Ford Wood Parts, cabriolet, car, catalogs, classic, clubs, closed cab truck, convertible, convertible-coupe, classic, car, catalogs, car parts, classic ford auto part, blind back, classic ford part, antique parts, early, early ford part, 3 window, 5 window, five window, ford wood parts catalog, fordwood, ford auto part, ford model a, ford model a part, ford parts, ford phaeton, flivver, ford, fordor, hardwoods, leatherback, modle a, modle t, model a, model t, Model A, Model-A, model a, Murray, murray, Model T, Model-T, model t, mustangs, oak, old, order, three window, tin lizzie, touring, driverside, parts, passengerside, phaeton, pickup, reproductions, roadster, reproduction, reproductions, restoration, restore, roadster, rod,  pickup, roadster-pickup, T-wood, t-Wood, tudor, 1928 model a ford, 1929 ford model a, 1929 model a ford, 1930 model a, 1931 model a ford, , model a, model t, 60ab, 60c, steelback, stock, street, reproduction, model a ford, model a ford part, model A part, obsolete ford part, prices, pricelists, per pieces, V8 parts, v8 wood, 1932, 1934, 1936, viccy, victoria, Viccy, vintage auto part, vintage car part, vintage ford auto parts, vintage ford parts, vintage part, wood">
+<meta http-equiv="description" content="Ford Wood for Antique Fords Cars 1913 through 1936. Making reproduction old Ford wood parts since 1963.  We make new wood for antique Ford Model T, Model A, and classic V8 Ford autocars">
+<meta name="GENERATOR" content="Microsoft FrontPage 6.0">
+<meta name="ProgId" content="FrontPage.Editor.Document">
+<title>Ford Wood - Reproduction wood parts for Model T, Model A and V8 Fords 
+from 1913 to 1936</title>
+
+<meta name="Microsoft Border" content="t">
+</head>
+
+<body background="images/back-ashwood.jpg" vlink="#800000" alink="#008000"><!--msnavigation--><table border="0" cellpadding="0" cellspacing="0" width="100%"><tr><td>
+
+<p align="center"><strong><font face="Maiandra GD" size="5">Wood for Fords / 
+Fordwood</font><font face="Maiandra GD" size="5" color="#800000"> </font>
+</strong></p>
+<p align="center">&nbsp;</p>
+
+</td></tr><!--msnavigation--></table><!--msnavigation--><table dir="ltr" border="0" cellpadding="0" cellspacing="0" width="100%"><tr><!--msnavigation--><td valign="top">
+<div align="center"><center>
+
+<table border="2" width="600" bordercolor="#800000" bordercolorlight="#800000" bordercolordark="#800000" cellpadding="0" style="border-collapse: collapse" cellspacing="0" bgcolor="#FFFFFF">
+  <tr>
+    <td width="171" valign="top" height="22" align="center">
+    <a href="faqs.htm">
+    <font color="#0000CC">
+    <img border="1" src="images/roadsigns-nopass.gif" width="43" height="46"></font></a></td>
+    <td width="171" valign="top" colspan="2" align="center" height="22">
+    <font face="Maiandra GD">
+    <a href="which_t_do_you_havep1.htm">
+    <font color="#0000CC">
+    <img src="images/anim-model-t.gif" alt="model-t-small.gif (4157 bytes)" WIDTH="45" HEIGHT="45" loop="infinite" border="1"></font></a></font></td>
+    <td width="171" valign="top" align="center">
+    <strong><a href="a_info.htm">
+    <font color="#0000CC" face="Maiandra GD">
+    <img src="images/modelaRoad_small.jpg" width="66" height="31" alt="modelaRoad_small.jpg (1992 bytes)" border="1"></font></a></strong></td>
+    <td width="171" valign="top" colspan="2" align="center">
+    <font face="Maiandra GD"><span style="mso-spacerun: yes"><a href="coupe-id.htm">
+    <font color="#0000CC">
+    <img src="images/modela5wc.jpg" width="66" height="33" alt="modela5wc.jpg (3354 bytes)" border="1"></font></a></span></font></td>
+    <td width="171" valign="top" colspan="2" align="center">
+    <font face="Maiandra GD">
+    <b><font color="#0000CC">&nbsp; </font><a href="links.htm"><font color="#0000CC">LINKS</font></a></b></font></td>
+    <td width="171" valign="top" align="center">
+    <font face="Maiandra GD">
+    <a href="fordor_body_styles.htm">
+    <font color="#0000CC">
+    <img src="images/modelAfordor2.jpg" width="62" height="32" alt="modelAfordor2.jpg (3467 bytes)" border="1"></font></a></font></td>
+    <td width="171" valign="top" align="center" height="22">
+    <font face="Maiandra GD">
+    <a href="v8_ford_information.htm">
+    <font color="#0000CC">
+    <img src="images/anim-hotrodV8.gif" alt="hotrodV8.gif (4922 bytes)" WIDTH="64" HEIGHT="26" loop="infinite" loopdelay="70" border="1"></font></a></font></td>
+    <td width="171" valign="top" align="center" height="22">
+    <a href="bodyblocks.htm">
+    <font color="#0000CC">
+    <img border="1" src="images/anim-earthsquared.gif" width="48" height="48"></font></a></td>
+    <td width="171" valign="top" align="center" height="22">
+    <a href="orderform.htm">
+    <img border="1" src="images/anim-dollarspin.gif" width="63" height="48"></a></td>
+  </tr>
+  <tr>
+    <td width="78" valign="top" height="22">
+    <p align="center">
+    <b><span lang="en-us"><font face="Maiandra GD" size="2">
+    <a href="faqs.htm"><font color="#0000CC">READ</font></a><font color="#0000CC">
+	<u>ME</u></font></font></span></b></td>
+    <td width="78" valign="top" colspan="2" align="center" height="22">
+    <b>
+    <font face="Maiandra GD" size="2"><font color="#0000CC">&quot;</font><a href="which_t_do_you_have.htm"><font color="#0000CC">T&quot; Info</font></a></font></b></td>
+    <td width="171" valign="top" align="center">
+    <strong><a href="a_info.htm">
+    <font face="Maiandra GD" color="#0000CC" size="2">&quot;A&quot; Info</font></a></strong></td>
+    <td width="171" valign="top" colspan="2" align="center">
+    <span style="mso-spacerun: yes; font-weight:700">
+    <font face="Maiandra GD" color="#800000" size="2"><a href="coupe-id.htm">
+    <font color="#0000CC">Coupes</font></a></font></span></td>
+    <td width="170" valign="top" colspan="2" align="center" height="22">
+    <a href="links.htm">
+    <font color="#0000CC">
+    <img border="0" src="images/anim-chain.gif" width="91" height="22"></font></a></td>
+    <td width="177" valign="top" align="center">
+    <b>
+    <font face="Maiandra GD" size="2"><a href="fordor_body_styles.htm">
+    <font color="#0000CC">Fordor</font></a><u><font color="#0000CC">s</font></u></font></b></td>
+    <td width="97" valign="top" align="center" height="22">
+    <font face="Maiandra GD">
+    <b>
+    <font size="2">
+    <a href="v8_ford_information.htm">
+    <font color="#0000CC">V8 Info</font></a></font></b></font><b><font color="#0000CC" face="Maiandra GD" size="2"> </font></b></td>
+    <td width="343" valign="top" align="center" height="22">
+    <font face="Maiandra GD">
+    <b><font size="2"><a href="bodyblocks.htm"><font color="#0000CC">A Blocks</font></a></font></b></font></td>
+    <td width="448" valign="top" align="center" height="22">
+    <font face="Maiandra GD">
+    <b><span lang="en-us"><a href="orderform.htm">
+    <font size="2" color="#0000CC">Order Form</font></a></span></b></font></td>
+  </tr>
+  <tr>
+    <td width="667" valign="top" colspan="12" align="center" height="21">
+    <p class="MsoNormal"><b><font face="Maiandra GD" color="#008000" size="4">We pay USA 
+ground shipping on <u>Full Kits</u> in the lower 48 States</font></b></td>
+  </tr>
+  <tr>
+    <td width="700" valign="top" colspan="12" align="left">
+    <p align="center">
+    <blink><font face="Maiandra GD" color="#0000CC"><b><font size="4">Below</font></b></font><font face="Maiandra GD"><font color="#0000CC"><font size="4">
+    </font><b>is our</b> <b><font size="4">Catalog</font></b> <b>with
+    <font size="4">200+ pages</font></b> <b>of information</b></font><b><font color="#000000"><br>
+    </font> </b>
+	<font color="#008000"><b><font size="4">Download</font> </b>
+	</font> 
+	<font color="#008000"><b>Drawings and find Prices and Information for <font size="4">FREE! </font> 
+	</b>
+    <font size="4">&nbsp; </font>
+    </font>
+    <br>
+    <b><font color="#800000">Check out the </font><a href="faqs.htm">
+    READ ME</a></b><font color="#800000"> <b>page for</b> <b>shipping 
+    and other information</b></font></font></blink></td>
+  </tr>
+  <tr>
+    <td align="center" width="671" valign="bottom" colspan="12" bgcolor="#FFFFFF">
+    <blink>
+    <b><font size="4" color="#800000" face="Maiandra GD">
+    <img border="0" src="images/hand2down.gif" width="12" height="30"> Click a 
+	Year &amp; Body Style 
+    from the List Below</font></b><font face="Maiandra GD" color="#000000"><b><font size="2"> </font></b></font>
+    </blink></td>
+  </tr>
+  <tr>
+    <td colspan="7" width="408" bordercolorlight="#800000" bordercolordark="#800000" height="23" valign="bottom" align="left">
+    <p align="left">
+    <b>
+    <a href="roadsters1913-27.htm">
+    <font face="Maiandra GD" color="#0000CC">Roadsters = 1913-1927</font></a></b></td>
+    <td colspan="5" width="421" rowspan="17" valign="top" align="center" height="354"><p align="center">
+    <font face="Comic Sans MS">
+    &nbsp;&nbsp;&nbsp;
+    <a href="do-u-avebriggs2murray.htm">
+    <img src="images/fordwood-car.JPG" alt="fordwood-car.JPG (27856 bytes)" width="265" height="185" align="center" border="1"></a>&nbsp;
+    <br>
+    </font>
+    <br>
+    <b>
+	<font face="Maiandra GD" size="4">
+    <a href="links.htm" style="text-decoration: none">
+	The Cubels</a></font><br>
+    <font face="Maiandra GD" color="#0000FF">
+    <font style="font-size: 11pt">Henry and Anna</font><br>
+    <font size="1">2nd Generation</font><font style="font-size: 9pt"> </font>
+	<font size="1">Family Owned and Operated </font>
+    </font><font face="Maiandra GD" color="#0000FF">
+    <br>
+    </font><font face="Maiandra GD">&nbsp;Phone: 435-749-0037<br>
+    Fax: 435-687-9239</font><font face="Maiandra GD" color="#0000CC"><br>
+    </font><font face="Maiandra GD" color="#800000">P.O. Box 1401&nbsp; </font>
+    </b>
+    <font color="#800000">
+    <strong><font face="Maiandra GD"><br>
+    </font>
+    </strong></font>
+    <b>
+    <font face="Maiandra GD" color="#800000">&nbsp;&nbsp;Huntington, Utah 84528
+	<a href="mailto:cubel_fordwood@yahoo.com" style="text-decoration: none">
+	cubel_fordwood@yahoo.com</a></font></b><p align="center">
+    &nbsp;</td>
+  </tr>
+  <tr>
+    <td colspan="7" width="408" bordercolorlight="#800000" bordercolordark="#800000" valign="bottom" align="left" height="21">
+    <b>
+    <font face="Maiandra GD" color="#800000"><a href="roadsters1928-37.htm">
+    <font color="#0000CC">Roadsters = 1928-1936</font></a></font></b></td>
+  </tr>
+  <tr>
+    <td colspan="7" width="408" bordercolorlight="#800000" bordercolordark="#800000" valign="bottom" align="left" height="21">
+    <b>
+    <a href="tourings.htm">
+    <font face="Maiandra GD" color="#0000CC">Tourings = 1913-1927</font></a></b></td>
+  </tr>
+  <tr>
+    <td colspan="7" width="408" bordercolorlight="#800000" bordercolordark="#800000" align="left" height="21">
+    <b>
+    <font face="Maiandra GD" color="#800000"><a href="phaetonpage.htm">
+    <font color="#0000CC">Phaetons</font></a></font><font face="Maiandra GD" color="#0000CC"></a></font><a href="phaetonpage.htm"><font face="Maiandra GD" color="#0000CC"> 
+    = 1928-1936</font></a></b></td>
+  </tr>
+  <tr>
+    <td colspan="7" valign="bottom" width="408" align="left" height="21">
+    <b>
+    <a href="pickups.htm">
+    <font face="Maiandra GD" color="#0000CC">Pickups=1928-1931 </font>
+    </a></b></td>
+  </tr>
+  <tr>
+    <td colspan="7" valign="bottom" width="408" align="left" height="21">
+    <b>
+    <a href="coupes.htm">
+    <font face="Maiandra GD" color="#0000CC">Standard Coupes = 1918-1931</font></a></b></td>
+  </tr>
+  <tr>
+    <td colspan="7" valign="bottom" width="408" align="left" height="20">
+    <b>
+    <a href="sportcoupes.htm">
+    <font face="Maiandra GD" color="#0000CC">Sport Coupes = 1928-1932</font></a></b></td>
+  </tr>
+  <tr>
+    <td colspan="7" valign="bottom" width="408" align="left" height="20">
+    <b>
+    <a href="specbuscoupes.htm">
+    <font face="Maiandra GD" color="#0000CC">Spec. &amp; Bus. Coupe = 1928-1929</font></a></b></td>
+  </tr>
+  <tr>
+    <td colspan="7" valign="bottom" width="408" align="left" height="20"><b>
+    <a href="coupe3-5win.htm">
+    <font face="Maiandra GD" color="#0000CC">3 Win Coupes 3&amp;5_Win 
+    Coupes</font></a></b></td>
+  </tr>
+  <tr>
+    <td colspan="7" width="408" valign="bottom" align="left" height="20">
+    <b>
+    <a href="viccys.htm">
+    <font face="Maiandra GD" color="#0000CC">Victoria Coupes = 1930-1934</font></a></b></td>
+  </tr>
+  <tr>
+    <td colspan="7" width="408" valign="bottom" align="left" height="20">
+    <b>
+    <a href="cabriolets.htm">
+    <font face="Maiandra GD" color="#0000CC">Cabriolets = 1929-1931</font></a></b></td>
+  </tr>
+  <tr>
+    <td colspan="7" width="408" valign="bottom" align="left" height="20"><b>
+    <a href="ttudors.htm">
+    <font face="Maiandra GD" color="#0000CC">Center Doors Sedan = 1915-1922</font></a></b></td>
+  </tr>
+  <tr>
+    <td colspan="7" width="408" valign="bottom" align="left" height="20">
+    <b>
+    <a href="ttudors.htm"><font face="Maiandra GD" color="#0000CC">Tudors = 1923-1927</font></a></b></td>
+  </tr>
+  <tr>
+    <td colspan="7" width="408" valign="bottom" align="left" height="20">
+    <b>
+    <a href="tudors8va.htm">
+    <font face="Maiandra GD" color="#0000CC">Tudors = 1928-1936</font></a></b></td>
+  </tr>
+  <tr>
+    <td colspan="7" width="408" valign="bottom" align="left" height="20">
+    <b>
+    <font face="Maiandra GD" color="#800000"><a href="fordors.htm">
+    <font color="#0000CC">Fordors = 
+    1923-1931</font></a></font><font face="Maiandra GD" color="#0000CC">/</font><font color="#800000"><a href="fordors.htm"><font color="#0000CC" face="Maiandra GD">Murray</font></a></font><font face="Maiandra GD" color="#0000CC">/</font><font color="#800000"><a href="fordors.htm"><font color="#0000CC" face="Maiandra GD">Slant</font></a></font><font face="Maiandra GD" color="#0000CC"></a></font></b></td>
+  </tr>
+  <tr>
+    <td colspan="7" width="408" valign="bottom" align="left" height="20">
+    <b>
+    <a href="briggs_fordors.htm">
+    <font face="Maiandra GD" color="#0000CC">Fordors &quot;Briggs&quot; = 1928-1931</font></a></b></td>
+  </tr>
+  <tr>
+    <td colspan="7" width="408" valign="top" align="left" height="10">
+    <b>
+    <a href="v8fordors.htm">
+    <font face="Maiandra GD" color="#0000CC">Fordors = 1932-1936</font></a></b></td>
+  </tr>
+  <tr>
+    <td colspan="2" width="398" align="center" valign="top" height="55">
+    <font face="Comic Sans MS" color="#0000CC">
+    <img src="images/anim-ccards.gif" alt="animatedccards.gif (3896 bytes)" width="71" height="51" border="1"></font></td>
+    <td colspan="2" width="402" align="center" valign="top" height="55">
+    <font face="Comic Sans MS">
+    <strong><span style="COLOR: #993300">
+    <a href="faqs.htm">
+    <font color="#0000CC">
+    <img src="images/guarantee.gif" alt="guarantee.gif (1290 bytes)" WIDTH="86" HEIGHT="50" border="1"></font></a></span></strong></font></td>
+    <td width="400" align="center" valign="top" height="55">
+    <font face="Maiandra GD">
+    <a href="http://www.ups.com/WebTracking/track">
+    <font color="#0000CC">
+    <img border="1" src="images/anim-upsman.gif" width="50" height="50"></font></a></font></td>
+    <td width="408" align="center" valign="top" colspan="2" height="55">
+    <b>
+    <font face="Maiandra GD" size="2">
+    <font color="#0000CC">About<br>
+    </font><a href="C4Cubel.htm"><font color="#0000CC">F</font></a></font><a href="C4Cubel.htm"><font face="Maiandra GD" size="1" color="#0000CC">ordwood</font></a><font face="Maiandra GD" color="#800000"> </font></b></td>
+    <td colspan="2" align="center" width="300" valign="middle" height="55">
+    <font color="#0000CC">
+    <a href="http://www.wunderground.com/US/UT/Huntington.html?bannertypeclick=gizmotimetemp">
+<img src="http://banners.wunderground.com/weathersticker/gizmotimetemp/language/www/US/UT/Huntington.gif" border=0
+alt="Click for Huntington, Utah Forecast" height=41 width=127></a></font></a></td>
+    <td align="center" width="167" valign="top" height="55">
+    <font face="Maiandra GD" color="#0000CC">
+    <img border="0" src="images/made_usa.gif" width="61" height="46"></font><td colspan="2" align="center" width="542" valign="top" height="55">
+    <a href="buggies.htm">
+    <img border="1" src="images/horse-buggy.JPG" width="75" height="52"></a></tr>
+  </table>
+</center></div>
+
+<!--msnavigation--></td></tr><!--msnavigation--></table></body>
+</html>
